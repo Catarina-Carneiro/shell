ea7fa9c8687ad7871db26e8a895d62ccba42ea19
CATARINA <catarina.carneiro@bandtec.com>
2020-11-28 21:22:12 -0300

    aaaaaa

---
+++ ShellScript.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+which java | grep -q /bin/java
+if [ $? = 0 ]
+
+
+then echo "Realizando execução do Java"
+        java -jar Continua3-1.0-SNAPSHOT-jar-with-dependencies.jar 
+else echo "Realiazndo instalação do Java"
+        sudo apt install curl
+        sudo apt install curl
+        curl -s “https://get.sdkman.io” | bash
+        source “$HOME/.sdkman/bin/sdkman-init.sh”
+        sdk install java 8.0.265.j9-adpt
+        echo "Realizando execução do Java"
+        java -jar Continua3-1.0-SNAPSHOT-jar-with-dependencies.jar 
+fi
